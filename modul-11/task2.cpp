8ee7e5649765c2401d5d6ddb996cad659bfe27de
Denis Sushilov <denissushilov8@gmail.com>
2026-06-09 16:59:26 +0500

    added function find_sign in task2.cpp

--- modul-11/task2.cpp
+++ modul-11/task2.cpp
@@ -4,15 +4,45 @@
     Имя файла: task2.cpp
 */
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::cin;
 using std::endl;
 
+bool valid_email(const std::string email);
+bool find_sign(const std::string str, const char ch, int& pos);
+
 int main()
 {
+    std::string email;
+
+    cout << "Input your email address: ";
+    cin >> email;
+ 
+
+    //cout << (valid_email(email) ? "Yes" : "No");
   
 
     return 0;
 }
 
+bool valid_email(const std::string email)
+{
+    return true;
+}
+
+bool find_sign(const std::string str, const char ch, int& pos)
+{
+    int counter{0};
+    for(char wr_ch : str) {
+        if(wr_ch == ch) {
+            pos = counter;
+            return true;
+        }
+        counter++;
+    }
+   
+    pos = -1;
+    return false;
+}
