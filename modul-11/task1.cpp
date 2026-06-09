5347b877f600c3694309abca8949a28092a61d95
Denis Sushilov <denissushilov8@gmail.com>
2026-06-09 16:00:09 +0500

    decided tast 1

--- modul-11/task1.cpp
+++ modul-11/task1.cpp
@@ -4,6 +4,10 @@
     Имя файла: task1.cpp
 */
 #include <iostream>
+#include <string>
+#include <cctype>
+
+void encrypt_caesar(std::string& str, int shift);
 
 using std::cout;
 using std::cin;
@@ -11,8 +15,33 @@ using std::endl;
 
 int main()
 {
+
+    std::string str;
+    int shift;
+    cout << "Enter text: ";
+    getline(cin, str);
+
+    cout << "Enter shift: ";
+    cin >> shift;
+
+    encrypt_caesar(str, shift);
+
+    cout << "Your cipher: " << str;
    
 
     return 0;
+}
+
+void encrypt_caesar(std::string& str, int shift)
+{
+    shift = (shift% 26 + 26) % 26;
+
+    for(char& ch: str) {
+        if(isalpha(ch)) {
+            char base = (isupper(ch) ? 'A' : 'a');
+
+            ch = base + (ch - base + shift) % 26;
+        }
+    }
 }
 
